5ffafae490cca2affc8302cbc87cda154b9b9ddd
Sahana Balaji <114191735+Sana47443@users.noreply.github.com>
2025-03-03 02:11:09 +0000

    Finishing up...

--- .eleventy.js
+++ .eleventy.js
@@ -1,8 +1,5 @@
 
-// The export statement makes these settings available to other files in 11ty
-
 module.exports = function(eleventyConfig) {
-  // ✅ Allows Eleventy to copy the styles and scripts to the output folder
   eleventyConfig.addPassthroughCopy("styles");
   eleventyConfig.addPassthroughCopy("script.js");
 
